b8454d35acdbe7398322bdc547c00c27297b0e59
nguyenxuancong <cafe4it@gmail.com>
2019-03-21 01:17:17 +0700

    rewrite seed data

--- index.js
+++ index.js
@@ -7,8 +7,6 @@ const _ = require('lodash');
 let app = express();
 fetch.Promise = Bluebird;
 
-const today = new Date()
-today.setHours(0, 0, 0, 0)
 
 nSQL('Albums').model([
     {key: 'id', type: 'int', props: ['pk']},
@@ -23,14 +21,22 @@ nSQL('Photos').model([
     {key: 'albumId', type: 'int'}
 ])
 
-nSQL('Reports').model([
+nSQL('Reports_Albums').model([
     {key: 'id', type: 'string', props: ['pk']},
     {key: 'downloadedDate', type: 'int'},
     {key: 'albumId', type: 'int'},
     {key: 'totalDownload', type: 'int'}
 ])
 
-function generateReport(){
+nSQL('Reports_Photos').model([
+    {key: 'id', type: 'string', props: ['pk']},
+    {key: 'viewedDate', type: 'int'},
+    {key: 'photoId', type: 'int'},
+    {key: 'totalViews', type: 'int'}
+])
+
+function generateReport(today){
+    today = today || (new Date()).setHours(0,0,0,0)
     const report = {
         albumId: _.random(1, 50),
         totalDownload: _.random(10, 1000),
@@ -51,8 +57,9 @@ nSQL().config({
                 .then((data) => {
                     _.each(data, (d) => {
                         const album = _.omit(d, 'userId')
-                        nSQL('Albums').query('upsert', album).exec().then((rows) => console.log(rows))
+                        nSQL('Albums').query('upsert', album).exec()
                     })
+                    console.log('seed Albums done!')
                 })
         }
     })
@@ -63,33 +70,101 @@ nSQL().config({
                 .then((res) => res.json())
                 .then((data) => {
                     _.each(data, (d) => {
-                        nSQL('Photos').query('upsert', d).exec().then((rows) => console.log(rows))
+                        nSQL('Photos').query('upsert', d).exec()
                     })
+                    console.log('seed Photos done!')
                 })
         }
     })
 
-
-    nSQL('Reports').query('select').exec().then((rows) => {
-        if (rows.length <= 0) {
-            for (let i = 0; i <= 50; i++) {
-                const report = generateReport();
-                nSQL('Reports').query('upsert', report).exec().then(rows => console.log(rows))
+    const tables = ['Reports_Albums', 'Reports_Photos']
+    _.each(tables, (t) => {
+        nSQL(t).query('select').exec().then((rows) => {
+            if (rows.length <= 0) {
+                for (let i = -100; i < 0; i++) {
+                    const today = new Date();
+                    today.setDate(i)
+                    today.setHours(0, 0,0,0)
+                    for(let j = 1; j <= 1000; j++){
+                        let report = {}
+                        if(t === 'Reports_Albums'){
+                            report = {
+                                albumId: _.random(1, 50),
+                                totalDownload: _.random(10, 1000),
+                                downloadedDate: today.getTime()
+                            }
+                            report.id = `${report.downloadedDate}!${report.albumId}`
+                        }else{
+                            report = {
+                                photoId: _.random(1, 5000),
+                                totalViews: _.random(10, 1000),
+                                viewedDate: today.getTime()
+                            }
+                            report.id = `${report.viewedDate}!${report.photoId}`
+                        }
+                        nSQL(t).query('upsert', report).exec()
+                    }
+                }
             }
-        }
+        })
+        console.log(`seed ${t} done!`)
     })
+
 
     app.get('/', function (req, res) {
         try {
-            nSQL('Reports')
-                .query('select', ['Albums.id AS id', 'Albums.title AS title', 'Reports.totalDownload AS totalDownload'])
-                .join({
-                    type: 'inner',
-                    table: 'Albums',
-                    where: ['Reports.albumId', '=', 'Albums.id']
-                }).orderBy({'totalDownload': 'desc'}).exec().then((rows) => {
-                res.json(rows)
-            })
+            const cmd = req.query['cmd']
+            const limit = req.query['limit'] || 10
+            const offset = req.query['limit'] || 0
+            if(cmd){
+                switch (cmd) {
+                    case 'summary':
+                        const tables = ['Albums', 'Photos', 'Reports_Albums', 'Reports_Photos']
+                        const promises = _.map(tables, (t) => {
+                            return nSQL(t).query('select', [`COUNT(*) AS ${t}.records`]).exec()
+                        })
+                        Promise.all(promises).then((data) => {
+                            res.json(_.chain(data).flatten().reduce((a, b) => _.assign(a, b)).value())
+                        })
+                        break;
+                    case 'top_albums_downloaded':
+                        nSQL('Reports_Albums')
+                            .query('select', ['Albums.id AS id', 'Albums.title AS title', 'SUM(Reports_Albums.totalDownload) AS totalDownload'])
+                            .join({
+                                type: 'inner',
+                                table: 'Albums',
+                                where: ['Reports_Albums.albumId', '=', 'Albums.id']
+                            })
+                            .groupBy({'Albums.id': 'asc'})
+                            .orderBy({'totalDownload': 'desc'})
+                            .limit(limit)
+                            .offset(offset)
+                            .exec()
+                            .then((rows) => {
+                                res.json(rows)
+                        })
+                        break;
+                    case 'top_photos_views':
+                        nSQL('Reports_Photos')
+                            .query('select', ['Photos.*', 'SUM(Reports_Photos.totalViews) AS totalViews'])
+                            .join({
+                                type: 'inner',
+                                table: 'Photos',
+                                where: ['Reports_Photos.photoId', '=', 'Photos.id']
+                            })
+                            .groupBy({'Photo.id': 'asc'})
+                            .orderBy({'totalViews': 'desc'})
+                            .limit(limit)
+                            .offset(offset)
+                            .exec()
+                            .then((rows) => {
+                                res.json(rows)
+                            })
+                        break;
+                }
+            }
+
+
         } catch (e) {
             console.error(e)
         }
@@ -98,11 +173,5 @@ nSQL().config({
     const port = 3000;
     app.listen(port, function () {
         console.log('App listening on port:' + port);
-
-        //simulator users download albums
-        setInterval(function () {
-            const report = generateReport();
-            nSQL('Reports').query('upsert', report).exec().then(() => console.log('User downloading...album ' + report.albumId))
-        }, _.random(500, 5000))
     })
 })
